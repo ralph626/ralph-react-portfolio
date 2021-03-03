edc6e0d109d070a7a8b50e18c503221169f247f1
Rafael_O <ralph.orozco@gmail.com>
2021-03-02 20:08:30 -0800

    updated images and fixed the banner with my name on it

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Typed from "typed.js";
 import Card from "./components/Card";
+import image1 from "./images/project1pic.PNG";
+import image2 from "./images/HomePage.jpg";
+import image3 from "./images/CardView.jpg";
 
 function App() {
-  const [text, setText] = useState([
-    `Hi, my name is Ralph! I'm a fullstack developer.`,
-  ]);
+  const projects = [
+    {
+      title: "Project 1",
+      url: image1,
+      desc: "another description",
+    },
+    {
+      title: "Home Page",
+      url: image2,
+    },
+    {
+      title: "Home Page",
+      url: image3,
+    },
+  ];
+  const [text, setText] = useState(
+    `Hi, my name is Ralph! I'm a fullstack developer.`
+  );
+
   useEffect(() => {
     window.addEventListener("load", () => {
       let typed = new Typed("#banner", {
         strings: [
           `Hi, my name is Ralph! I'm a fullstack developer.`,
-          ...text,
+          text,
           "I like to party",
         ],
         typeSpeed: 30,
@@ -31,21 +50,27 @@ function App() {
       </div>
       <div className="container" style={{ marginTop: "100px" }}>
         <div className="row">
-          <div className="col-sm">
-            <Card
-              title="Project 1"
-              url="https://upload.wikimedia.org/wikipedia/en/2/28/Pok%C3%A9mon_Bulbasaur_art.png"
-            />
+          {projects.map((project, i) => (
+            <div className="col" key={i}>
+              <Card
+                title={project.title}
+                url={project.url}
+                desc={project.desc}
+              />
+            </div>
+          ))}
+          {/* <div className="col-sm">
+            <Card title="Project 1" url={image1} />
           </div>
           <div className="col-sm">
             <Card
-              url="https://cdn.bulbagarden.net/upload/7/73/004Charmander.png"
-              desc="This is the best project ever!"
+              url="Cardview.jpg"
+              desc="This is my teams Project! Parc-It "
             />
           </div>
           <div className="col-sm">
-            <Card url="http://static.pokemonpets.com/images/monsters-images-800-800/7-Squirtle.png" />
-          </div>
+            <Card url="HomePage.jpg" desc="This is Parc-It Home page" />
+          </div> */}
         </div>
       </div>
     </>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,24 @@
 import "./Card.css";
-export default function Card(props) {
+export default function Card({ title, url, desc }) {
   return (
     <>
       <div className="card shadow-sm rounded" style={{ width: "20rem" }}>
-        <img src={props.url} className="card-img-top" height="300" alt="..." />
+        <div
+          className="card-img-top"
+          style={{
+            height: "300px",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundImage: "url(" + url + ")",
+          }}
+        ></div>
+        {/* <img src={props.url} className="card-img-top" height="300" alt="..." /> */}
         <div className="card-body">
-          <h5 className="card-title">{props.title}</h5>
+          <h5 className="card-title">{title}</h5>
           <p className="card-text">
-            {props.desc ||
+            {desc ||
               `Some quick example text to build on the card title and make up the
-            bulk of the card's content.`}
+            bulk of the card's content. Our easy user friendly take on meet up applications.`}
           </p>
         </div>
       </div>
